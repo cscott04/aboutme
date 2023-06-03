fe235fdccd2b2f38998600ac011914aef41b8e22
constance scott <constancedscott05@yahoo.com>
2023-06-03 18:53:36 -0500

    added more notes

---
+++ aboutme.js
@@ -0,0 +1,57 @@
+'use strict';
+let person = prompt('Do I like Jackson?');
+person = person.toUpperCase();
+// console.log(person);
+if (person === 'YES' || person === 'Y') {
+  // console.log('No. I do not');
+  alert('No. I do not');
+} else if (person === 'NO' || person === 'N') {
+  // console.log("You're correct");
+  alert("You're correct");
+}
+
+let education = prompt('Did I graduate from MTSU?');
+education = education.toUpperCase();
+// console.log(education);
+if (education === 'YES' || education === 'Y') {
+  // console.log('That is correct');
+  alert('That is correct');
+} else if (education === 'NO' || education === 'N') {
+  // console.log("Sorry, you're wrong");
+  alert("Sorry, you're wrong");
+}
+let color = prompt('Is purple my favorite color?');
+color = color.toUpperCase();
+// console.log(color;
+if (color === 'YES' || color === 'Y') {
+  // console.log('That is correct');
+  alert('That is correct');
+} else if (color === 'NO' || color === 'N') {
+  // console.log("Sorry, you're wrong");
+  alert("Sorry, you're wrong");
+}
+let hobbies = prompt('Do I like exercising?');
+hobbies = hobbies.toUpperCase();
+// console.log(hobbies);
+if (hobbies === 'YES' || hobbies === 'Y') {
+  // console.log('That is correct');
+  alert('That is correct');
+} else if (hobbies === 'NO' || hobbies === 'N') {
+  // console.log("Sorry, you're wrong");
+  alert("Sorry, you're wrong");
+}
+let animal = prompt('Do I like dogs?');
+animal = animal.toUpperCase();
+// console.log(animal);
+if (animal === 'YES' || animal === 'Y') {
+  // console.log('That is correct');
+  alert('That is correct');
+} else if (animal === 'NO' || animal === 'N') {
+  // console.log("Sorry, you're wrong");
+  alert("Sorry, you're wrong");
+}
+let user = prompt('What is your name?');
+alert('Hello ' + user);
+
+// alert(person);
+// let color = prompt();
