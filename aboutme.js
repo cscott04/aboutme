bef52f1bd9a25cb8e62eebcff62e8936f7020ad9
constance scott <constancedscott05@yahoo.com>
2023-06-08 21:32:37 -0500

    updated assignment

--- aboutme.js
+++ aboutme.js
@@ -1,4 +1,5 @@
 'use strict';
+let score = 0;
 let person = prompt('Do I like Jackson?');
 person = person.toUpperCase();
 // console.log(person);
@@ -8,6 +9,7 @@ if (person === 'YES' || person === 'Y') {
 } else if (person === 'NO' || person === 'N') {
   // console.log("You're correct");
   alert("You're correct");
+  score++;
 }
 
 let education = prompt('Did I graduate from MTSU?');
@@ -16,6 +18,7 @@ education = education.toUpperCase();
 if (education === 'YES' || education === 'Y') {
   // console.log('That is correct');
   alert('That is correct');
+  score++;
 } else if (education === 'NO' || education === 'N') {
   // console.log("Sorry, you're wrong");
   alert("Sorry, you're wrong");
@@ -26,6 +29,7 @@ color = color.toUpperCase();
 if (color === 'YES' || color === 'Y') {
   // console.log('That is correct');
   alert('That is correct');
+  score++;
 } else if (color === 'NO' || color === 'N') {
   // console.log("Sorry, you're wrong");
   alert("Sorry, you're wrong");
@@ -36,6 +40,7 @@ hobbies = hobbies.toUpperCase();
 if (hobbies === 'YES' || hobbies === 'Y') {
   // console.log('That is correct');
   alert('That is correct');
+  score++;
 } else if (hobbies === 'NO' || hobbies === 'N') {
   // console.log("Sorry, you're wrong");
   alert("Sorry, you're wrong");
@@ -46,12 +51,31 @@ animal = animal.toUpperCase();
 if (animal === 'YES' || animal === 'Y') {
   // console.log('That is correct');
   alert('That is correct');
+  score++;
 } else if (animal === 'NO' || animal === 'N') {
   // console.log("Sorry, you're wrong");
   alert("Sorry, you're wrong");
 }
-let user = prompt('What is your name?');
-alert('Hello ' + user);
-
+let number = '';
+let i = 0;
+while (number !== '5') {
+  number = prompt('Guess a number betwwen 1 and 10');
+  if (number < '5') {
+    alert('to low');
+  } else if (number > '5') {
+    alert('too high');
+  }
+  i++;
+  if (i === 5) {
+    number = '5';
+  }
+}
+let singers = ['monica', 'fantasia', 'whitney houston'];
+let answer = prompt('Guess my favorite singer').toLowerCase();
+if (answer === singers[0] || answer === singers[1] || answer === singers[2]) {
+  alert(answer + ' is correct');
+  score++;
+}
+alert('you scored ' + score);
 // alert(person);
 // let color = prompt();
